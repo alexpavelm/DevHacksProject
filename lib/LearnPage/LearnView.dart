b5a17c01fa864373f5f48f209b5d252838c88144
Ioana ALEXANDRU <ioana.alexandru@stud.acs.upb.ro>
2019-11-23 17:46:06 +0200

    STONKS

--- lib/LearnPage/LearnView.dart
+++ lib/LearnPage/LearnView.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:bubble/bubble.dart';
 
 class LearnView extends StatefulWidget {
   @override
-  _LearnViewState createState() => _LearnViewState();
+  _LearnViewState createState() => _LearnViewState(
+      content: Container(
+          child: Text('STONKS',
+              style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 30))));
 }
 
 class _LearnViewState extends State<LearnView> {
+  Container _content;
+
+  _LearnViewState({Container content}) {
+    _content = content;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Learn page")),
+    BubbleStyle styleTutorial = BubbleStyle(
+        stick: true,
+        color: Colors.grey[200],
+        elevation: 1,
+        margin: BubbleEdges.only(top: 20.0, bottom: 20.0),
+        alignment: Alignment.centerLeft,
+        shadowColor: Colors.blue);
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/stonks.png"),
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomLeft),
+        ),
+        child: Bubble(style: styleTutorial, child: _content),
+      ),
     );
   }
 }
