fee5ac30db3a4f150b9454007c57a3136dc81d1e
florin9 <dobosflorin98@gmail.com>
2019-11-23 18:48:44 +0200

    investmentView

--- lib/InvestmentPage/InvestmentView.dart
+++ lib/InvestmentPage/InvestmentView.dart
@@ -4,7 +4,12 @@ class InvestmentView extends StatefulWidget {
   @override
   _InvestmentViewState createState() => _InvestmentViewState();
 }
-
+class InvestmentData{
+  String name;
+  String description;
+  int points;
+  InvestmentData(this.name,this.description,this.points);
+}
 class _InvestmentViewState extends State<InvestmentView> {
   @override
   Widget build(BuildContext context) {
@@ -31,40 +36,101 @@ class _InvestmentViewState extends State<InvestmentView> {
   }
 
   Widget buildList() {
-    List<String> titles = new List();
-    titles.add("Google");
-    titles.add("Facebook");
-    titles.add("hardcoding is life");
+    List<InvestmentData> titles = new List();
+    titles.add(InvestmentData("Google","desc Google",87));
+    titles.add(InvestmentData("Facebook","desc Facebook",34));
+    titles.add(InvestmentData("Apple","desc Apple",65));
     return Column(
-      children: titles.map((data) => investBlock(data, data.length)).toList(),
+      children: titles.map((data) => investBlock(data.name, data.points, data.description)).toList(),
     );
   }
 
-  Widget investBlock(String title, int points) {
+  // foreach(list) => {return list.title list.desc list.}
+  Widget investBlock(String title, int points, String desc) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text(title, style: TextStyle(fontFamily: 'Avenir', fontSize: 22, fontWeight: FontWeight.w500, color: Colors.black54),),
-          investCard(title, points),
-          investCard(title, points),
-          investCard(title, points),
+          Text(
+            title,
+            style: TextStyle(
+                fontFamily: 'Avenir',
+                fontSize: 22,
+                fontWeight: FontWeight.w500,
+                color: Colors.black54),
+          ),
+          //random data
+          investCard(title, "desc", points),
+          investCard(title, "desc", points),
+          investCard(title, "desc", points),
         ],
       ),
     );
   }
-  Widget investCard(title, points) {
+
+  Widget investCard(title, description, points) {
     return Card(
       child: Container(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(20.0),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[Text(title, style: TextStyle(fontFamily: 'Avenir', fontSize: 18, fontWeight: FontWeight.w500),), Text(points.toString())],
-          ),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: TextStyle(
+                          fontFamily: 'Avenir',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Container(
+                      alignment: Alignment(-1,0),
+                      child: Text(
+                        description,
+                        style: TextStyle(
+                            fontFamily: 'Avenir',
+                            fontSize: 18,
+                            fontWeight: FontWeight.w100),
+                      ),
+                    )
+                  ],
+                ),
+                getBoxRounded(points)
+              ]),
         ),
       ),
     );
+  }
+
+  Widget getBoxRounded(int data) {
+    return Container(
+      padding: EdgeInsets.only(left: 12, top: 5, right: 12, bottom: 5),
+      child: Text(
+        data.toString() + "%",
+        style: TextStyle(
+          fontFamily: 'Avenir',
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+          color: Colors.white,
+        ),
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: getColorBasedOnScore(data)),
+    );
+  }
+
+  Color getColorBasedOnScore(int points) {
+    if (points > 80) {
+      return Colors.green;
+    }
+    if (points > 50) {
+      return Colors.orange;
+    }
+    return Colors.red;
   }
 }
